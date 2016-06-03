6bc1fc7d7b5112d1ef20905f550c213daeaf8fb6
Marko Kreen <markokr@gmail.com>
2016-06-03 14:23:14 +0300

    Lower version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name = "pgq",
     license = "ISC",
-    version = '4.0',
+    version = '3.3',
     maintainer = "Marko Kreen",
     maintainer_email = "markokr@gmail.com",
     py_modules = ['pgq', 'pgq.cascade'],
