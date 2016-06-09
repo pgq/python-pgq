d944efe3892a650ebaf21a077178953ba07ec894
Marko Kreen <markokr@gmail.com>
2016-06-09 13:55:59 +0300

    Fix setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version = '3.3',
     maintainer = "Marko Kreen",
     maintainer_email = "markokr@gmail.com",
-    py_modules = ['pgq', 'pgq.cascade'],
+    packages = ['pgq', 'pgq.cascade'],
     install_requires = ['skytools', 'psycopg2']
 )
 
