5fdf4815bebd3782232eef6e5c426c86a9a4c260
Marko Kreen <markokr@gmail.com>
2020-07-18 22:26:09 +0300

    tox: update, switch to pytest/pylint

--- tests/test_pgq.py
+++ tests/test_pgq.py
@@ -3,8 +3,6 @@ import pgq
 
 from skytools import natsort_key
 
-from nose.tools import *
-
 def test_version():
-    assert_true(natsort_key(pgq.__version__) >= natsort_key('3.3'))
+    assert natsort_key(pgq.__version__) >= natsort_key('3.3')
 
